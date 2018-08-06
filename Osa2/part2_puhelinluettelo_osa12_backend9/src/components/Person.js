741f383eb71639278b33f1d51a43e882e83fa91b
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2018-08-06 16:32:08 +0300

    Frontend puhelinluettelo modification for publishing with backend

---
+++ Osa2/part2_puhelinluettelo_osa12_backend9/src/components/Person.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Person = ({ person, onClick }) => {
+  return (
+    <tr>
+      <td>
+      {person.name}
+      </td>
+      <td>
+      {person.number}
+      </td>
+      <td>
+      <button onClick={onClick}>poista</button>
+      </td>
+    </tr>
+  )
+}
+
+export default Person
